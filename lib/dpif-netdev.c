6a5579195b10797abcf07d399499de7aae453095
masaki <m1117@aol.jp>
2025-04-26 22:22:33 +0900

    fix issue

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -5813,7 +5813,7 @@ send_packets(struct dp_packet_batch *batch)
 
     #ifdef DEBUG_INVESTIGATION
     openlog("KSL-IWAI", LOG_CONS | LOG_PID, LOG_USER);
-    syslog(LOG_WARNING, "@@ Packets in batch: %ln", &batch->count);
+    syslog(LOG_WARNING, "@@ Packets in batch: %zu", batch->count);
     #endif
 
     for (int packets = 0; packets < batch->count; packets++){
