1f7a02ed15d91f4150a5427817aa2d7db7b43bf8
masaki <m1117@aol.jp>
2025-06-29 02:45:01 +0900

    try

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -5577,7 +5577,7 @@ send_packets(struct dp_packet_batch *batch)
     // elapsed_us = (end.tv_sec - start.tv_sec) * 1000000L + (end.tv_usec - start.tv_usec);
     // syslog(LOG_WARNING, "@@ syslog use(us): %ld", elapsed_us);
 
-    usleep(10);
+    usleep(5);
 
     *((volatile char *)shm_ptr + SHM_FLAG_PACKETS) = 1;
 
@@ -5604,7 +5604,7 @@ send_packets(struct dp_packet_batch *batch)
 
     // show_flags();
 
-    usleep(10);
+    usleep(5);
 
     // syslog(LOG_WARNING, "@@ received");
     *((volatile char *)shm_ptr + SHM_FLAG_RESULTS) = 0;
