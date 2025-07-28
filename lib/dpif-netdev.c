2328eaa7d526ddea23db1f1754b58654df6065d8
masaki <m1117@aol.jp>
2025-07-28 09:35:51 +0900

    add wait

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -5624,7 +5624,7 @@ send_packets(struct dp_packet_batch *batch)
 
     // result
     while (*(shm_ptr + offset + SHM_FLAG_RESULTS) != 1) {
-        usleep(WAIT_TIME);
+        // usleep(WAIT_TIME);
     }
     // show_flags();
 
