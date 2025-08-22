fd424a0ff0be5f1b763d635c85fd5b371d694270
masaki <m1117@aol.jp>
2025-08-22 20:43:20 +0900

    remove wait

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -5531,7 +5531,7 @@ send_packets(struct dp_packet_batch *batch)
     
     // show_flags();
     while (*(shm_ptr + offset + SHM_FLAG_PACKETS) != 0) {
-        usleep(WAIT_TIME);
+        // usleep(WAIT_TIME);
     }
     // show_flags();
     memcpy(shm_ptr+offset+SHM_FLAG_HOW_MANY_PACKETS, &batch->count, sizeof(batch->count));
@@ -5626,7 +5626,7 @@ send_packets(struct dp_packet_batch *batch)
 
     // result
     while (*(shm_ptr + offset + SHM_FLAG_RESULTS) != 1) {
-        usleep(WAIT_TIME);
+        // usleep(WAIT_TIME);
     }
     // show_flags();
 
@@ -5647,7 +5647,7 @@ send_packets(struct dp_packet_batch *batch)
 
     // show_flags();
 
-    usleep(5);
+    // usleep(5);
 
     // syslog(LOG_WARNING, "@@ received / PID: %d, TID: %d", pid, tid);
     *((volatile char *)shm_ptr + offset + SHM_FLAG_RESULTS) = 0;
